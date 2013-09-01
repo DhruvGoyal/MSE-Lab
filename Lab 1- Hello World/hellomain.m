607e705a2f40157dd34b700a8c96cd5fede37299
Dhruv Goyal <dhruv_goyal1431@yahoo.com>
2013-09-01 13:43:40 +0530

    Update hellomain.m

--- Lab 1- Hello World/hellomain.m
+++ Lab 1- Hello World/hellomain.m
@@ -6,7 +6,7 @@
 int main(int argc,const char *argv[])
 {
 hello *h=[[hello alloc]init]; //create and initialize an object
-[h addStringValue:"Hello!"]; //send input string
+[h addStringValue:"Hello World!"]; //send input string
 [h print]; //print input string
 [h release]; //release the objects memory
 return 0;
