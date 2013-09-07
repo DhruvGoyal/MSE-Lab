72fb305ec6133b0fbaac667c39e27691bedb719b
Dhruv Goyal <dhruv_goyal1431@yahoo.com>
2013-09-07 18:39:24 +0530

    Update main.m

--- Lab 3 - Tax/main.m
+++ Lab 3 - Tax/main.m
@@ -9,14 +9,16 @@ int main(int argc, const char *argv[])
 	Goods *chair = [[Goods alloc]init];
 	Goods *carrot = [[Goods alloc]init];
 
-	[chair setType:@"finished goods"];
-	[chair setAmount:500.0];
-
 	[carrot setType:@"groceries"];
-	[carrot setAmount:100.0];
+	[carrot setAmount:200.0];
+	
+	[chair setType:@"finished goods"];
+	[chair setAmount:400.0];
 
-	NSLog(@"\nAmount without tax for chair: %.3lf\nAmount with tax for chair: %.3lf", [chair amount], [calc calcTax:chair]);
+	
 	NSLog(@"\nAmount without tax for carrot: %.3lf\nAmount with tax for carrot: %.3lf", [carrot amount], [calc calcTax:carrot]);
+	NSLog(@"\nAmount without tax for chair: %.3lf\nAmount with tax for chair: %.3lf", [chair amount], [calc calcTax:chair]);
+	
 
 	[pool release];
 
